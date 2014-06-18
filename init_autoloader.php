c3c6a258d51f48e179d2bda56e7516aebef1e123
jzang <jason.zang@live.com>
2014-06-18 13:19:35 +0800

    1st zf2 uploaded

---
+++ init_autoloader.php
@@ -0,0 +1,9 @@
+<?php
+
+include 'vendor/ZF2/library/Zend/Loader/AutoloaderFactory.php';
+Zend\Loader\AutoloaderFactory::factory(array(
+    'Zend\Loader\StandardAutoloader' => array(
+        'autoregister_zf' => true
+    )
+));
+
---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: onlyit
+ * Date: 14-6-18
+ * Time: 上午11:05
+ */
+
+$test = 0;
+
+while(++$test)
+{
+    echo $test; PHP_EOL;
+}
+
